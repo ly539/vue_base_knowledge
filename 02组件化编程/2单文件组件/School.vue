402f2a745d085fe86839095e4e0787c5aeddf819
ly539 <535261845@qq.com>
2023-10-22 22:47:02 +0800

    first commit

---
+++ 02组件化编程/2单文件组件/School.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- 1. 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称:{{schoolName}}</h2>
+        <h2>学校地址:{{address}}</h2>
+    </div>
+</template>
+
+<script>
+    //2. 组件交互相关的代码
+     export default{
+            name:'School',
+            data(){
+                return {
+                    schoolName:'scdx',
+                    address:'眉山'
+                }
+            }
+      }
+
+</script>
+
+<style>
+    /* 3. 组件的样式 */
+    .demo{
+        background-color: black;
+    }
+</style>
---
+++ 02组件化编程/2单文件组件/Student.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- 1. 组件的结构 -->
+    <div class="demo">
+        <h2>学生名称:{{name}}</h2>
+        <h2>年龄:{{age}}</h2>
+    </div>
+</template>
+
+<script>
+    //2. 组件交互相关的代码
+     export default{
+            name:'Student',
+            data(){
+                return {
+                    name:'ly',
+                    age:20
+                }
+            }
+      }
+
+</script>
+
+<style>
+    /* 3. 组件的样式 */
+    .demo{
+        background-color: black;
+    }
+</style>
